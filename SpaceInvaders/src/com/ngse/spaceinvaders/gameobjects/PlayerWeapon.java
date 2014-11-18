670d1d0ad59a2637971abf6a929dddec595be705
AlphaBravo314 <alpha.blumer@gmail.com>
2014-11-17 18:18:47 -0800

    moar tests

--- SpaceInvaders/src/com/ngse/spaceinvaders/gameobjects/PlayerWeapon.java
+++ SpaceInvaders/src/com/ngse/spaceinvaders/gameobjects/PlayerWeapon.java
@@ -12,6 +12,7 @@ public class PlayerWeapon {
 	
 	
 	
+	
 	//defines the number values of each weapon type
 	//the order of the weapons' values is the order in which they will be obtained through upgrading
 	//nothing can be less than BASIC or more than ULTIMATE
